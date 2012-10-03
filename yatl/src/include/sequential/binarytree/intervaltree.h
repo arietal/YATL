6779297dca5defba67b97f6942b39f1b6c87132a
arietal <tal_arie@yahoo.com>
2012-10-03 18:30:22 +0200

    Added copyright notice

--- yatl/src/include/sequential/binarytree/intervaltree.h
+++ yatl/src/include/sequential/binarytree/intervaltree.h
@@ -3,6 +3,24 @@
  *
  *  Created on: Jul 5, 2012
  *      Author: arietal
+ *
+ *  Copyright (c) 2012 Arie Tal. All rights reserved.
+ *
+ *  This file is part of Yet Another Template Library (YATL).
+ *
+ *  YATL is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  YATL is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with YATL.  If not, see <http://www.gnu.org/licenses/>.
+ *
  */
 
 #ifndef INTERVALTREE_H_
